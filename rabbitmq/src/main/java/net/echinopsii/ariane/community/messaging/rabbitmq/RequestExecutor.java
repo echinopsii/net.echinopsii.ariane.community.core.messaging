3334c42112d56ec955af783078d6af8333eebd99
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-11-30 19:01:00 +0100

    [ACC-500] check if beginWaitingAnswer>0 before defining slow rpc

--- rabbitmq/src/main/java/net/echinopsii/ariane/community/messaging/rabbitmq/RequestExecutor.java
+++ rabbitmq/src/main/java/net/echinopsii/ariane/community/messaging/rabbitmq/RequestExecutor.java
@@ -164,9 +164,9 @@ public class RequestExecutor extends MomAkkaAbsRequestExecutor implements MomReq
             long endWaitingAnswer = System.nanoTime();
             long rpcTime = endWaitingAnswer - beginWaitingAnswer;
             log.debug("RPC time : " + rpcTime);
-            if (super.getMomClient().getRPCTimout()>0 && rpcTime > super.getMomClient().getRPCTimout()*1000000000*3/5) {
+            if (super.getMomClient().getRPCTimout()>0 && beginWaitingAnswer>0 && rpcTime > super.getMomClient().getRPCTimout()*1000000000 * 3 / 5) {
                 destinationTrace.put(destination, true);
-                log.warn("Slow RPC time (" + rpcTime/1000000000 + ") on request to queue " + destination);
+                log.warn("Slow RPC time (" + rpcTime / 1000000000 + ") on request to queue " + destination);
             } else  destinationTrace.put(destination, false);
             response = new MsgTranslator().decode(new Message().setEnvelope(delivery.getEnvelope()).
                     setProperties(delivery.getProperties()).
